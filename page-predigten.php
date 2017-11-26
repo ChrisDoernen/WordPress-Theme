2a0cf126ecd51e9465db661d4efb506f1735ad01
Christian Dörnen <32569989+ChrisDoernen@users.noreply.github.com>
2017-11-26 12:40:07 +0100

    Add Path variables

--- page-predigten.php
+++ page-predigten.php
@@ -34,7 +34,7 @@
 					<div class="row">
                         <div class="col-xs-12 left">
                             <?php
-                                $files = glob('/www/htdocs/w00700fe/live_podcast/*.{mp3}', GLOB_BRACE);
+                                $files = glob('__pathToPodcastDirectory__/*.{mp3}', GLOB_BRACE);
                                 
                                 $sermons = [];
                                 foreach($files as $file) {
