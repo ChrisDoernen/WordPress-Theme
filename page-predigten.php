02a6d156048dd2e2ef73364d19761b86a995186a
ChrisDoernen <christian.doernen@icloud.com>
2020-03-21 18:34:53 +0100

    Delete events history page and constraint sermons to 10

--- page-predigten.php
+++ page-predigten.php
@@ -34,12 +34,12 @@
 					<div class="row">
                         <div class="col-xs-12 left">
                             <?php
+
+								$regularExpression = "/\d{4}-\d{2}-\d{2}_.+_.+\.mp3/";
                                 $sermonDirectory = get_home_path().'/../live_podcast/*/*.mp3';
                                 $sermons = [];
-                                
+								
                                 foreach(glob($sermonDirectory) as $file) {
-                                	
-                                	$regularExpression = "/\d{4}-\d{2}-\d{2}_.+_.+\.mp3/";
                                 	if(preg_match($regularExpression, $file) === 1)
                                 	{
 	                                    $split = explode("_", basename($file));
@@ -47,14 +47,22 @@
 	                                    $sermon['preacher'] = $split[1];
 	                                    $sermon['fileName'] = basename($file);
 	                                    $sermon['title'] = explode(".", $split[2])[0];
-	                                    array_push($sermons, $sermon);
+										array_push($sermons, $sermon);
+										$index++;
                                 	}
                                 }
                                 
                                 rsort($sermons);
-                                
+								
+								$index = 0;
+								$maxSermons = 10;
                                 foreach($sermons as $sermon) {
-                                    include (get_template_directory().'/card-sermon.php');
+									if ($index == $maxSermons) {
+										break;
+									}
+
+									include (get_template_directory().'/card-sermon.php');
+									$index++;
                                 }
                             ?>
                         </div>
